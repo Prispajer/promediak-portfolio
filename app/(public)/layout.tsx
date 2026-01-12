458c1f69756df8766081c9ca498dbb8897c71783
Adrian Kozieł <koziel.adrian98@gmail.com>
2026-01-12 23:45:53 +0100

    feat(assets): Added images and favicon for application, deleted unused images

---
+++ app/(public)/layout.tsx
@@ -0,0 +1,6 @@
+﻿import React from "react";
+import AppLayout from "@/layout/AppLayout/AppLayout";
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+    return <AppLayout>{children}</AppLayout>;
+}
