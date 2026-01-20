336e3f9ebba7a6169419011e7e7c807e597d5a14
Adrian Kozieł <koziel.adrian98@gmail.com>
2026-01-20 23:34:25 +0100

    feat(ui): Added reusable ImageBackground component

---
+++ components/common/ImageBackground.tsx
@@ -0,0 +1,14 @@
+﻿import {StaticImageData} from "next/image";
+
+type ImageBackgroundProps = {
+    backgroundImage: StaticImageData;
+    className?: string;
+};
+
+const ImageBackground = ({backgroundImage, className}: ImageBackgroundProps) => {
+    return (
+        <div className={`absolute inset-0 ${className}`} style={{backgroundImage: `url(${backgroundImage.src})`}}/>
+    );
+}
+
+export default ImageBackground;
