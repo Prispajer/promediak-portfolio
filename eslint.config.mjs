0e32a6d91d8db9ca1843aa11eeacbc5a61224326
Adrian Kozieł <koziel.adrian98@gmail.com>
2026-01-24 23:31:52 +0100

    chore(app): Added eslint + prettier config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,21 +1,32 @@
-// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
 import storybook from "eslint-plugin-storybook";
-
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
 
+import react from "eslint-plugin-react";
+import reactHooks from "eslint-plugin-react-hooks";
+import importPlugin from "eslint-plugin-import";
+import unusedImports from "eslint-plugin-unused-imports";
+
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
-  // Override default ignores of eslint-config-next.
-  globalIgnores([
-    // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
+
+  {
+    plugins: {
+      react,
+      "react-hooks": reactHooks,
+      import: importPlugin,
+      "unused-imports": unusedImports,
+      storybook,
+    },
+    rules: {
+      "unused-imports/no-unused-imports": "error",
+      "react/react-in-jsx-scope": "off",
+    },
+  },
+
+  globalIgnores([".next/**", "out/**", "build/**", "next-env.d.ts"]),
 ]);
 
 export default eslintConfig;
