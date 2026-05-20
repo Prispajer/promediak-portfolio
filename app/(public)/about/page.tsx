5a39cd3ea02657e2608d62f7080166b0b23d4d77
Adrian Kozieł <koziel.adrian98@gmail.com>
2026-05-20 22:02:10 +0200

    refactor(about): Removed redundant about-us page and associated logic

--- app/(public)/about/page.tsx
+++
@@ -1,9 +0,0 @@
-﻿import AboutContainer from "@/components/sections/About/AboutContainer";
-
-export default function AboutPage() {
-  return (
-    <div className="bg-background min-h-screen">
-      <AboutContainer />
-    </div>
-  );
-}
