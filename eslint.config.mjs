1fd81c36ffe26abdb4675b3b9c4d486fbfbbbc6e
Adrian Kozieł <koziel.adrian98@gmail.com>
2026-05-23 01:24:12 +0200

    chore(deps): Updated project dependencies

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,27 +1,13 @@
-import storybook from "eslint-plugin-storybook";
-import { defineConfig, globalIgnores } from "eslint/config";
+﻿import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
-
-import react from "eslint-plugin-react";
-import reactHooks from "eslint-plugin-react-hooks";
-import importPlugin from "eslint-plugin-import";
-import unusedImports from "eslint-plugin-unused-imports";
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
 
   {
-    plugins: {
-      react,
-      "react-hooks": reactHooks,
-      import: importPlugin,
-      "unused-imports": unusedImports,
-      storybook,
-    },
     rules: {
-      "unused-imports/no-unused-imports": "error",
       "react/react-in-jsx-scope": "off",
     },
   },
